b8ab1793de704cc3323976e5cfada344e2e45770
Alexandru Toma <alexandru.toma15@imperial.ac.uk>
2018-01-25 18:11:25 +0000

    Created skeleton files

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+#!/usr/bin/env python
+
 import scipy.io as spio
 
-mat = spio.loadmat('Data/cleandata_students.mat', squeeze_me=True)
-x = mat['x']
-y = mat['y']
-print (x.size)
-print (y)
+
+def main():
+    mat = spio.loadmat('Data/cleandata_students.mat', squeeze_me=True)
+    x = mat['x']
+    y = mat['y']
+    print (x.size)
+    print (y)
+
+
+def decision_tree_learning(examples, attributes, binary_targets):
+    pass
+
+
+def majority_value(binary_targets):
+    pass
+
+
+def choose_best_decision_attribute(examples, attributes, binary_targets):
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ tree.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+class Tree:
+    tree_op = -1
+    tree_kids = []
+    tree_class = -1
