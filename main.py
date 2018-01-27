e1648ccc8721bfb778beb45dce065d0b4f3c398e
Rishabh Jain <wakeuprj@gmail.com>
2018-01-27 12:43:33 +0000

    Continued impl.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import scipy.io as spio
+from tree import TreeNode
 
 
 def main():
@@ -15,19 +16,31 @@ def main():
     attributes = []
     for i in range(total_attributes):
         attributes.append(i+1)
-
+    y = [1,1,2,2,2,2,4,4,4,4,4,4]
     # Parent call to recursive function
     decision_tree_learning(x, attributes, y)
 
 
 def decision_tree_learning(examples, attributes, binary_targets):
-    pass
+    if same_binary_targets(binary_targets):
+        return TreeNode(binary_targets[0])
+    elif len(attributes) == 0:
+        return TreeNode(majority_value(binary_targets))
+    else:
+        best_attribute = choose_best_decision_attribute(examples, attributes, binary_targets)
+        # root_attribute = TreeNode()
 
-def check_same_targets(binary_targets):
-    pass
+def same_binary_targets(binary_targets):
+    if len(binary_targets) <= 0:
+        return True
+    first_target = binary_targets[0]
+    for target in binary_targets:
+        if target != first_target:
+            return False
+    return True
 
 def majority_value(binary_targets):
-    pass
+    return max(set(binary_targets), key=binary_targets.count)
 
 
 def choose_best_decision_attribute(examples, attributes, binary_targets):
--- tree.py
+++ tree.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
-class Tree:
-    tree_op = -1
-    tree_kids = []
-    tree_class = -1
+class TreeNode:
+    #
+    # def __init__(self):
+    #     self.op = -1
+    #     self.kids = []
+    #     self.label = -1
+    def __init__(self, label):
+        self.label = label
