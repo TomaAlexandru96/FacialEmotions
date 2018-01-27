95649c21e2cd6e60324559935364f167e687405e
vinamragrawal <vinamragrawal@gmail.com>
2018-01-27 12:14:42 +0000

    started implementation for decision tree learning

--- main.py
+++ main.py
@@ -4,16 +4,27 @@ import scipy.io as spio
 
 
 def main():
+    total_attributes = 45
+
+    # Extract data from the file
     mat = spio.loadmat('Data/cleandata_students.mat', squeeze_me=True)
     x = mat['x']
     y = mat['y']
-    print (x.size)
-    print (y)
+
+    # Create attributes
+    attributes = []
+    for i in range(total_attributes):
+        attributes.append(i+1)
+
+    # Parent call to recursive function
+    decision_tree_learning(x, attributes, y)
 
 
 def decision_tree_learning(examples, attributes, binary_targets):
     pass
 
+def check_same_targets(binary_targets):
+    pass
 
 def majority_value(binary_targets):
     pass
