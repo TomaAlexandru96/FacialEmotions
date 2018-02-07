6912656c2638f3abc977f5fc2e6bb0426d593121
Rishabh Jain <wakeuprj@gmail.com>
2018-02-07 19:17:44 +0000

    Finished evaluation(sort of).

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import scipy.io as spio
 from tree import TreeNode
 import math
+import random
+import numpy as np
 
 
 def main():
     clean_data = 'Data/cleandata_students.mat'
     noisy_data = 'Data/noisydata_students.mat'
     total_attributes = 45
-    number_of_trees = 6
+    number_of_emotions = 6
+    number_of_trees = number_of_emotions
+    k_folds = 10
+    randomise = False
 
     # Extract data from the file
     mat = spio.loadmat(clean_data, squeeze_me=True)
@@ -15,73 +20,118 @@ def main():
     y = list(mat['y'])
 
     # Create attributes
-    attributes = [0] * 45
+    attributes = [0] * total_attributes
     for i in range(total_attributes):
         attributes[i] = i + 1
+    perc_acc = [0] * k_folds
+    # k-fold cross-validation
+    for i in range(k_folds):
+        test_data_input = []
+        test_data_output = []
+        traing_data_input = []
+        traing_data_output = []
+        validation_data_input = []
+        validation_data_output = []
+        for j in range(len(x)):
+            if (j % k_folds == i):
+                test_data_input.append(x[j])
+                test_data_output.append(y[j])
+            elif (j % k_folds == (i+1) % k_folds):
+                validation_data_input.append(x[j])
+                validation_data_output.append(y[j])
+            else:
+                traing_data_input.append(x[j])
+                traing_data_output.append(y[j])
 
-    # trees = []
-    # # Parent call to recursive function
-    # for i in range(1, number_of_trees + 1):
-    #     # the binary target for index i
-    #     y_tree = list(map(lambda value: value == i if 1 else 0, y))
-    #     trees.append(decision_tree_learning(x, attributes, y_tree))
-    #
-    # # analyse_data(number_of_trees, x, y)
-    # robust_validation(number_of_trees, trees, x, y)
-    validation(number_of_trees, attributes, x, y)
 
-
-def analyse_data(number_of_trees, x, y):
-    emotions_count = [0] * 6
-    for i in range(len(x)):
-        emotions_count[y[i] - 1] += 1
-    print(str(emotions_count))
-
-
+        tree_priority = [0] * number_of_trees
+        if randomise:
+            unvalidated_trees = train_trees(number_of_trees, attributes, traing_data_input, traing_data_output)
+            tree_priority = get_tree_priority(unvalidated_trees, validation_data_input, validation_data_output)
+        trees = train_trees(number_of_trees, attributes, traing_data_input + validation_data_input, traing_data_output + validation_data_output)
+        predictions = test_trees(trees, test_data_input, tree_priority, randomise)
+        perc_acc[i] = evaluate_results(predictions, test_data_output)
+    print(np.mean(perc_acc))
 def robust_validation(number_of_trees, trees, x, y):
     trees_FP = [0] * number_of_trees
     trees_FN = [0] * number_of_trees
     for i in range(len(x)):
         for t in range(number_of_trees):
-            actual = trees[t].parse_tree(x[i])
-            if y[i] == t+1 and not actual:
-                # print("Failed FN: " + str(x[i]))
-                # print("For emotion: " + str(y[i]))
+            tree_output = trees[t].parse_tree(x[i])
+            if y[i] == t+1 and not tree_output:
                 trees_FN[t] += 1
-            elif y[i] != t+1 and actual:
-                # print("Failed FP: " + str(x[i]))
-                # print("For emotion: " + str(y[i]))
+            elif y[i] != t+1 and tree_output:
                 trees_FP[t] += 1
 
-    print("FN: " + str(trees_FN))
-    print("FP: " + str(trees_FP))
-    print()
+def evaluate_results(predictions, actual_outputs):
+    correct_cases = 0
+    incorrect_cases = 0
+    for k in range(len(predictions)):
+        if (predictions[k] == actual_outputs[k]):
+            correct_cases += 1
+        else:
+            incorrect_cases += 1
+    total = correct_cases + incorrect_cases
+    perc_correct = (correct_cases / total) * 100
+    perc_incorrect = (incorrect_cases / total) * 100
+    return perc_correct
 
-    # dump_tree("6", trees[5])
+def train_trees(number_of_trees, attributes, traing_data_input, traing_data_output):
+    trees = []
+    # Parent call to recursive function
+    for i in range(1, number_of_trees + 1):
+        # the binary target for index i
+        y_tree = list(map(lambda value: value == i if 1 else 0, traing_data_output))
+        trees.append(decision_tree_learning(traing_data_input, attributes, y_tree))
+    return trees
 
-def validation(number_of_trees, attributes, x, y):
-    for i in range(10):
-        test_data_input = []
-        test_data_output = []
-        traing_data_input = []
-        traing_data_output = []
-        for j in range(len(x)):
-            if (j % 10 == i):
-                test_data_input.append(x[j])
-                test_data_output.append(y[j])
-            else:
-                traing_data_input.append(x[j])
-                traing_data_output.append(y[j])
-        trees = []
-        # Parent call to recursive function
-        for i in range(1, number_of_trees + 1):
-            # the binary target for index i
-            y_tree = list(map(lambda value: value == i if 1 else 0, traing_data_output))
-            trees.append(decision_tree_learning(traing_data_input, attributes, y_tree))
+def get_tree_priority(trees, validation_data_input, validation_data_output):
+    number_of_trees = len(trees)
+    tree_priority = [0] * number_of_trees
+    for t in range(number_of_trees):
+        tree = trees[t]
+        tree_priority[t] = get_perc_accuracy(tree, t + 1, validation_data_input, validation_data_output)
+    return tree_priority
 
-        robust_validation(number_of_trees, trees, test_data_input, test_data_output)
+def get_perc_accuracy(tree, emotion_val, x, y):
+    correct = 0
+    for i in range(len(x)):
+        output = tree.parse_tree(x[i])
+        if (y[i] == emotion_val and output) or (y[i] !=emotion_val and (not output)):
+            correct += 1
+    return correct / len(x)
 
+def test_trees(trees, test_data, tree_priority, randomise):
+    number_of_trees = len(trees)
+    final_result = [0] * len(test_data)
+    for i in range(len(test_data)):
+        test_case_output = [0] * number_of_trees
+        for t in range(number_of_trees):
+            output = trees[t].parse_tree(test_data[i])
+            test_case_output[t] = output
+        if randomise:
+            final_result[i] = get_emotion_val_rand(test_case_output)
+        else:
+            final_result[i] = get_emotion_val(test_case_output, tree_priority)
+    return final_result
 
+def get_emotion_val_rand(output):
+    trues = []
+    for i in range(len(output)):
+        if output[i]:
+            trues.append(i)
+    if len(trues) == 0:
+        return random.randint(1, len(output))
+    return random.choice(trues) + 1
+
+def get_emotion_val(output, tree_priority):
+    trues = []
+    for i in range(len(output)):
+        if output[i]:
+            trues.append(tree_priority[i])
+        else:
+            trues.append(-tree_priority[i])
+    return trues.index(max(trues)) + 1
 
 def decision_tree_learning(examples, attributes, binary_targets):
     if same_binary_targets(binary_targets):
@@ -108,7 +158,6 @@ def decision_tree_learning(examples, attributes, binary_targets):
                 tree.add_kid(subtree)
                 attributes.append(best_attribute)
         return tree
-
 
 # checks if binary_targets vector contains same values
 def same_binary_targets(binary_targets):
